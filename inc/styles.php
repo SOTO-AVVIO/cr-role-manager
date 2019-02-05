1dcf77d19796f391ce545c1c541d5e2f418e2c17
Mahbub <mmhasaneee@gmail.com>
2019-02-05 14:01:59 +0600

    Import CR specific changes

--- inc/styles.php
+++ inc/styles.php
@@ -1,136 +1,134 @@
 <style type="text/css">
-	.crrm-hide {
-		display: none!important;
-	}
-	#last-edit {display: none;}
-	#acf-group_5afa865e60317 {display: none!important;}
-	.slate-pro-admin .term-description-wrap div.mce-panel iframe {
-		border-left: 1px solid #e5e5e5;
-		border-right: 1px solid #e5e5e5;
-		box-sizing: border-box;
-	}
-	.upload-php .media-toolbar.wp-filter .view-switch .view-list {display: none!important;}
-	.upload-php a[data-aio-name="collection"] {display: none!important;}
-	.attachment-info .actions a {display: none!important;}
-	.attachment-info .actions {color: transparent;}
-	.attachment-info .actions .delete-attachment {margin-left: -15px;}
-	.media-modal .imgedit-group.imgedit-applyto {display: none!important;}
-	.vc_navbar-header #vc_logo {display: none!important;}
-	.vc_controls-row .vc_row_layouts {display: none!important;}
-	.vc_controls-row .column_add {display: none!important;}
-	.vc_controls-row .column_edit {display: none!important;}
-	.vc_control-column > .vc_column-edit {display: none!important;}
-	.vc_ui-panel-header .vc_ui-panel-header-controls .vc_ui-dropdown {
-		display: none!important;
-	}
-	.mce-toolbar .mce-btn-group .mce-btn[aria-label="Gallery from Media Folder"],
-	.mce-toolbar .mce-btn-group .mce-btn[aria-label="Distraction-free writing mode"] {
-		display: none!important;
-	}
-	#wp-link .vc-link-nofollow {
-		display: none!important;
-	}
-	.term-php #wpseo_meta {
-		display: none;
-	}
-	#nav-menu-footer .major-publishing-actions .delete-action {
-		display: none!important;
-	}
-	.crrm-screen-page-edit .page-title-action,
-	.crrm-screen-post-edit .page-title-action,
-	.crrm-screen-accommodation-edit .page-title-action,
-	.crrm-screen-amenity-edit .page-title-action,
-	.crrm-screen-apartment-edit .page-title-action,
-	.crrm-screen-apartment-edit .page-title-action,
-	.edit-php .page-title-action {
-		display: none!important;
-	}
-	#menu-posts li.cr-submenu-item-add-new,
-	#menu-pages li.cr-submenu-item-add-new,
-	#menu-posts-accommodation li.cr-submenu-item-add-room,
-	#menu-posts-amenity li.cr-submenu-item-add-new-amenity,
-	#menu-posts-apartment li.cr-submenu-item-add-new-apartment,
-	#menu-posts-offer li.cr-submenu-item-add-new-offer,
-	#menu-media .cr-submenu-item-add-new {
-		display: none!important;
-	}
-	
-	#vc_no-content-helper #vc_no-content-add-text-block,
-	#vc_no-content-helper #vc_templates-more-layouts{
-		display: none!important;
-	}
-	
-	.vc_control-column .vc_column-delete {
-		display: none!important;
-	}
-	[data-element_type="cr_hero_slider"] .vc_controls .column_delete,
-	[data-element_type="cr_property_slider"] .vc_controls .column_delete,
-	[data-element_type="cr_fixed_width_grid"] .vc_controls .column_delete,
-	[data-element_type="cr_full_width_grid"] .vc_controls .column_delete, 
-	[data-element_type="cr_expanding_images"] .vc_controls .column_delete,
-	[data-element_type="cr_gallery"] .vc_controls .column_delete{
-		display: none!important;
-	}
-	.bottom-controls .column_add {
-		display: none!important;
-	}
-	.crrm-grayedout-element {
-		
-	}
-	.crrm-grayedout-element::after {
-		content: "";
-		display: block;
-		position: absolute;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		background: #fff;
-		opacity: .55;
-		cursor: not-allowed;
-		z-index: 10;
-	}
-	[data-element_type="cr_title_icons"] .wpb_element_wrapper,
-	[data-element_type="cr_accommodations"] .wpb_element_wrapper,
-	[data-element_type="cr_enquiry_form"] .wpb_element_wrapper,
-	[data-element_type="cr_sitemap"] .wpb_element_wrapper,
-	[data-element_type="cr_instagram_feed"] .wpb_element_wrapper,
-	[data-element_type="cr_allora_integration"] .wpb_element_wrapper,
-	[data-element_type="cr_gravity_form"] .wpb_element_wrapper,
-	[data-element_type="cr_title_icons"] .wpb_element_wrapper {
-		position: relative;
-	}
-	[data-element_type="cr_title_icons"] .wpb_element_wrapper::after,
-	[data-element_type="cr_accommodations"] .wpb_element_wrapper::after,
-	[data-element_type="cr_enquiry_form"] .wpb_element_wrapper::after,
-	[data-element_type="cr_sitemap"] .wpb_element_wrapper::after,
-	[data-element_type="cr_instagram_feed"] .wpb_element_wrapper::after,
-	[data-element_type="cr_allora_integration"] .wpb_element_wrapper::after,
-	[data-element_type="cr_gravity_form"] .wpb_element_wrapper::after,
-	[data-element_type="cr_title_icons"] .wpb_element_wrapper::after {
-		position: absolute;
-		content: "";
-		width: 100%;
-		height: 100%;
-		left: 0;
-		top: 0;
-		z-index: 10;
-		background: #fff;
-		opacity: .55;
-	}
-	[data-element_type="cr_title_icons"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_accommodations"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_enquiry_form"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_sitemap"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_instagram_feed"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_allora_integration"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_gravity_form"] .vc_controls .vc_control-btn:not(.vc_element-move),
-	[data-element_type="cr_title_icons"] .vc_controls .vc_control-btn:not(.vc_element-move) {
-		display: none!important;
-	}
-	.wp-media-buttons .button.wdi_thickbox,
-	.wp-media-buttons #add_gform.button{
-		display: none!important;
-	}
-	 
+    .crrm-hide {
+        display: none!important;
+    }
+    #last-edit {display: none;}
+    #acf-group_5afa865e60317 {display: none!important;}
+    .slate-pro-admin .term-description-wrap div.mce-panel iframe {
+        border-left: 1px solid #e5e5e5;
+        border-right: 1px solid #e5e5e5;
+        box-sizing: border-box;
+    }
+    .upload-php .media-toolbar.wp-filter .view-switch .view-list {display: none!important;}
+    .upload-php a[data-aio-name="collection"] {display: none!important;}
+    .attachment-info .actions a {display: none!important;}
+    .attachment-info .actions {color: transparent;}
+    .attachment-info .actions .delete-attachment {margin-left: -15px;}
+    .media-modal .imgedit-group.imgedit-applyto {display: none!important;}
+    .vc_navbar-header #vc_logo {display: none!important;}
+    .vc_controls-row .vc_row_layouts {display: none!important;}
+    .vc_controls-row .column_add {display: none!important;}
+    .vc_controls-row .column_edit {display: none!important;}
+    .vc_control-column > .vc_column-edit {display: none!important;}
+    .vc_ui-panel-header .vc_ui-panel-header-controls .vc_ui-dropdown {
+        display: none!important;
+    }
+    .mce-toolbar .mce-btn-group .mce-btn[aria-label="Gallery from Media Folder"],
+    .mce-toolbar .mce-btn-group .mce-btn[aria-label="Distraction-free writing mode"] {
+        display: none!important;
+    }
+    #wp-link .vc-link-nofollow {
+        display: none!important;
+    }
+    .term-php #wpseo_meta {
+        display: none;
+    }
+    #nav-menu-footer .major-publishing-actions .delete-action {
+        display: none!important;
+    }
+    .crrm-screen-page-edit .page-title-action,
+    .crrm-screen-post-edit .page-title-action,
+    .crrm-screen-amenity-edit .page-title-action,
+    .crrm-screen-apartment-edit .page-title-action,
+    .crrm-screen-apartment-edit .page-title-action,
+    .edit-php .page-title-action {
+        display: none!important;
+    }
+    #menu-posts li.cr-submenu-item-add-new,
+    #menu-pages li.cr-submenu-item-add-new,
+    #menu-posts-amenity li.cr-submenu-item-add-new-amenity,
+    #menu-posts-apartment li.cr-submenu-item-add-new-apartment,
+    #menu-posts-offer li.cr-submenu-item-add-new-offer,
+    #menu-media .cr-submenu-item-add-new {
+        display: none!important;
+    }
+
+    #vc_no-content-helper #vc_no-content-add-text-block,
+    #vc_no-content-helper #vc_templates-more-layouts{
+        display: none!important;
+    }
+
+    .vc_control-column .vc_column-delete {
+        display: none!important;
+    }
+    [data-element_type="cr_hero_slider"] .vc_controls .column_delete,
+    [data-element_type="cr_property_slider"] .vc_controls .column_delete,
+    [data-element_type="cr_fixed_width_grid"] .vc_controls .column_delete,
+    [data-element_type="cr_full_width_grid"] .vc_controls .column_delete,
+    [data-element_type="cr_expanding_images"] .vc_controls .column_delete,
+    [data-element_type="cr_gallery"] .vc_controls .column_delete{
+        display: none!important;
+    }
+    .bottom-controls .column_add {
+        display: none!important;
+    }
+    .crrm-grayedout-element {
+
+    }
+    .crrm-grayedout-element::after {
+        content: "";
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: #fff;
+        opacity: .55;
+        cursor: not-allowed;
+        z-index: 10;
+    }
+    [data-element_type="cr_title_icons"] .wpb_element_wrapper,
+    [data-element_type="cr_accommodations"] .wpb_element_wrapper,
+    [data-element_type="cr_enquiry_form"] .wpb_element_wrapper,
+    [data-element_type="cr_sitemap"] .wpb_element_wrapper,
+    [data-element_type="cr_instagram_feed"] .wpb_element_wrapper,
+    [data-element_type="cr_allora_integration"] .wpb_element_wrapper,
+    [data-element_type="cr_gravity_form"] .wpb_element_wrapper,
+    [data-element_type="cr_title_icons"] .wpb_element_wrapper {
+        position: relative;
+    }
+    [data-element_type="cr_title_icons"] .wpb_element_wrapper::after,
+    [data-element_type="cr_accommodations"] .wpb_element_wrapper::after,
+    [data-element_type="cr_enquiry_form"] .wpb_element_wrapper::after,
+    [data-element_type="cr_sitemap"] .wpb_element_wrapper::after,
+    [data-element_type="cr_instagram_feed"] .wpb_element_wrapper::after,
+    [data-element_type="cr_allora_integration"] .wpb_element_wrapper::after,
+    [data-element_type="cr_gravity_form"] .wpb_element_wrapper::after,
+    [data-element_type="cr_title_icons"] .wpb_element_wrapper::after {
+        position: absolute;
+        content: "";
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        z-index: 10;
+        background: #fff;
+        opacity: .55;
+    }
+    [data-element_type="cr_title_icons"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_accommodations"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_enquiry_form"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_sitemap"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_instagram_feed"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_allora_integration"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_gravity_form"] .vc_controls .vc_control-btn:not(.vc_element-move),
+    [data-element_type="cr_title_icons"] .vc_controls .vc_control-btn:not(.vc_element-move) {
+        display: none!important;
+    }
+    .wp-media-buttons .button.wdi_thickbox,
+    .wp-media-buttons #add_gform.button{
+        display: none!important;
+    }
+
 </style>
